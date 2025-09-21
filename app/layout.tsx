873abad297dd8b15d3682f3f56785f66cb23c7a9
ok777casino <username@users.noreply.github.com>
2025-09-21 10:44:54 +0800

    ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export const viewport: Viewport = {
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
-  themeColor: '0E141C',
+  themeColor: '#0E141C',
 }
 
 export default function RootLayout({
