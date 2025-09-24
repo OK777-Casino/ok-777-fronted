c016774f98f0068d1e528b28c7b4a08f7076c4f4
TgmBet <tgm.bet@outlook.com>
2025-09-24 09:50:38 +0800

    button ui

--- components/Header.tsx
+++ components/Header.tsx
@@ -379,7 +379,7 @@ const MobileGameNav: React.FC<MobileGameNavProps> = ({
   }, [activeTab, gameNavTabs])
 
   return (
-    <div className="lg:hidden p-2">
+    <div className="lg:hidden px-2 py-1">
       <Swiper
         modules={[FreeMode]}
         freeMode={true}
@@ -397,7 +397,7 @@ const MobileGameNav: React.FC<MobileGameNavProps> = ({
                 swiperRef.current?.slideTo(idx, 250)
               }}
               variant={activeTab === tab.id ? 'primary' : 'secondary'}
-              className="h-0 py-4 px-2 whitespace-nowrap min-w-fit"
+              className="!h-1.25 py-4 px-2 whitespace-nowrap min-w-fit"
             >
               <img src={tab.icon} alt={tab.label} className="w-5 h-5" />
               <span className="text-[0.8rem] font-bold">{tab.label}</span>
